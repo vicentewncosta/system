fb640f685497eb18ca47a8e20e0c99562a2e3529
Vicente Costa <89530296+vicentewncosta@users.noreply.github.com>
2023-05-23 16:27:22 -0300

    Update config.js

--- back-end/config/config.js
+++ back-end/config/config.js
@@ -1,22 +1,4 @@
-/* eslint-disable max-len */
-/* !
 
-=========================================================
-* Argon React NodeJS - v1.0.0
-=========================================================
-
-* Product Page: https://argon-dashboard-react-nodejs.creative-tim.com/
-* Copyright 2020 Creative Tim (https://https://www.creative-tim.com//)
-* Copyright 2020 ProjectData (https://projectdata.dev/)
-* Licensed under MIT (https://github.com/creativetimofficial/argon-dashboard-react-nodejs/blob/main/README.md)
-
-* Coded by Creative Tim & ProjectData
-
-=========================================================
-
-* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-
-*/
 module.exports = {
   smtpConf: {
     host: 'smtp.googlemail.com', // Gmail Host
